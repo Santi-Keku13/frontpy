31df221b9e23fea2aa348e325709ba59658c72cb
Bojack8789 <Francoferraro18@gmail.com>
2026-04-20 12:20:51 -0300

    Arreglando tuner

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   
   // URL BASE
   const BASE_URL = import.meta.env.PROD 
-    ? 'https://respondents-optics-onto-communist.trycloudflare.com'
+    ? 'https://short-tiffany-deserve-pie.trycloudflare.com'
     : 'http://localhost:5000';
   
   const API_URL = `${BASE_URL}/api`;
