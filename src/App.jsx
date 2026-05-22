3598ff0d6de6b11a3193cc75f9a5c331207a0a79
Bojack8789 <Francoferraro18@gmail.com>
2026-05-22 08:15:15 -0300

    Config tunel

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   
   // URL BASE
   const BASE_URL = import.meta.env.PROD 
-    ? 'https://prove-decide-generator-ser.trycloudflare.com'
+    ? 'https://boundary-elect-maps-club.trycloudflare.com'
     : 'http://localhost:5000';
   
   const API_URL = `${BASE_URL}/api`;
