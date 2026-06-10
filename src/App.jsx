688e59c4708aa3be2f3557ebecdc75d9fdc36f2f
Bojack8789 <Francoferraro18@gmail.com>
2026-06-10 08:37:38 -0300

    Agregando tunel

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   
   // URL BASE
   const BASE_URL = import.meta.env.PROD 
-    ? 'https://accessories-manga-drawings-howto.trycloudflare.com'
+    ? 'https://should-intimate-klein-garden.trycloudflare.com'
     : 'http://localhost:5000';
   
   const API_URL = `${BASE_URL}/api`;
